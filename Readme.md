f03e32928b1302355d4968aa2d3b1a15f9a1b3e2
Nigel Schuster <nigel.schusters@googlemail.com>
2018-06-22 11:27:08 -0400

    Improve setup

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# next-js
+
+...
+
+## Change log
+
+The change log can be found on the [Releases page](https://github.com/Neitsch/next-js/releases).
+
+## Contributing
+
+Everyone is welcome to contribute. Please take a moment to review the [contributing guidelines](Contributing.md).
+
+## Authors and license
+
+[Nigel Schuster](https://github.com/Neitsch/next-experiment) and [contributors](https://github.com/Neitsch/next-js/graphs/contributors).
+
+MIT License, see the included [License.md](License.md) file.
--- jest.config.js
+++ jest.config.js
@@ -1,80 +1,67 @@
 module.exports = {
-    collectCoverage: true,
-    collectCoverageFrom: [
-      '<rootDir>/{server,lib,pages}/**/*.?(!d.){ts,tsx}',
-    ],
-    coverageThreshold: {
-      "global": {
-        "branches": 100,
-        "functions": 100,
-        "lines": 100,
-        "statements": 100
-      }
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/{server,lib,pages}/**/*.?(!d.){ts,tsx}"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
-    globals: {
-      "ts-jest": {
-        useBabelrc: true,
-        enableTsDiagnostics: true
-      }
+  },
+  globals: {
+    "ts-jest": {
+      useBabelrc: true,
+      enableTsDiagnostics: true,
     },
-    projects: [
-      {
-        snapshotSerializers: ["enzyme-to-json/serializer"],
-        setupTestFrameworkScriptFile: "<rootDir>lib/setupTests.js",
-        displayName: 'test',
-        transform: {
-          // "^.+\\.jsx?$": "babel-jest",
-          "^.+\\.tsx?$": "ts-jest"
-        },
-        testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-        moduleFileExtensions: [
-          "ts",
-          "tsx",
-          "js",
-          "jsx",
-          "json",
-          "node"
-        ]
+  },
+  projects: [
+    {
+      snapshotSerializers: ["enzyme-to-json/serializer"],
+      setupTestFrameworkScriptFile: "<rootDir>lib/setupTests.js",
+      displayName: "test",
+      transform: {
+        // "^.+\\.jsx?$": "babel-jest",
+        "^.+\\.tsx?$": "ts-jest",
       },
-      {
-        runner: 'jest-runner-tslint',
-        displayName: 'lint',
-        moduleFileExtensions: [
-            "js",
-            "jsx",
-            "json",
-            "ts",
-            "tsx",
-            "css",
-            "less",
-            "scss",
-            "graphql",
-            "md",
-            "markdown"
-        ],
-        testMatch: [
-            '<rootDir>/{server,lib,pages}/**/*.{ts,tsx}'
-        ]
-      },
-      {
-        runner: 'jest-runner-prettier',
-        displayName: 'prettier',
-        moduleFileExtensions: [
-            "js",
-            "jsx",
-            "json",
-            "ts",
-            "tsx",
-            "css",
-            "less",
-            "scss",
-            "graphql",
-            "md",
-            "markdown"
-        ],
-        testMatch: [
-            '<rootDir>/{server,lib,pages}/**/*.{ts,tsx}'
-        ]
-      }
-    ]
-  }
+      testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+      moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    },
+    {
+      runner: "jest-runner-tslint",
+      displayName: "lint",
+      moduleFileExtensions: [
+        "js",
+        "jsx",
+        "json",
+        "ts",
+        "tsx",
+        "css",
+        "less",
+        "scss",
+        "graphql",
+        "md",
+        "markdown",
+      ],
+      testMatch: ["<rootDir>/{server,lib,pages}/**/*.{ts,tsx}"],
+    },
+    {
+      runner: "jest-runner-prettier",
+      displayName: "prettier",
+      moduleFileExtensions: [
+        "js",
+        "jsx",
+        "json",
+        "ts",
+        "tsx",
+        "css",
+        "less",
+        "scss",
+        "graphql",
+        "md",
+        "markdown",
+      ],
+      testMatch: ["<rootDir>/{server,lib,pages}/**/*.{ts,tsx}"],
+    },
+  ],
+};
