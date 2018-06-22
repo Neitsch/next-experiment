12d7ecdf21519bf9ebfc627d2584d429de8e0d16
Nigel Schuster <nigel.schusters@googlemail.com>
2018-06-22 10:59:28 -0400

    Improve test

--- server/__tests__/index.test.ts
+++ server/__tests__/index.test.ts
@@ -5,6 +5,7 @@ describe("Server", () => {
     let serverLauncher;
     let errorFunc;
     let setPort;
+    const parseFunc = jest.fn().mockImplementation(a => a);
     jest.doMock("../../typeDefs/nextShim", () => ({
       nextServer: () => ({
         getRequestHandler: () => reqHandler,
@@ -26,12 +27,19 @@ describe("Server", () => {
         };
       },
     }));
-    jest.doMock("url");
+    jest.doMock("url", () => ({
+      parse: parseFunc,
+    }));
     require("../index");
     serverLauncher();
     expect(reqHandler).toHaveBeenCalledTimes(0);
-    handlerFunc({ url: "http://www.abc.com/123" });
+    const url = "http://www.abc.com/123";
+    const urlCall = { url };
+    const otherArg = jest.fn();
+    handlerFunc(urlCall, otherArg);
     expect(reqHandler).toHaveBeenCalledTimes(1);
+    expect(reqHandler).toHaveBeenCalledWith(urlCall, otherArg, url);
+    expect(parseFunc).toHaveBeenCalledWith(url, true);
     expect(setPort).toEqual(3000);
     expect(() => {
       errorFunc(new Error(":)"));
