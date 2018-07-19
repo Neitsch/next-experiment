741dc1238d851dbd009f8f7b8f71b1df9a7dcd4c
Nigel Schuster <Neitsch@users.noreply.github.com>
2018-07-19 14:14:16 +0200

    E2E fix

--- e2e/home.test.ts
+++ e2e/home.test.ts
@@ -1,22 +1,22 @@
-const { Builder, until } = require("selenium-webdriver");
-const axeBuilder = require("axe-webdriverjs");
+import { Builder, until, WebDriver } from "selenium-webdriver";
+import { AxeBuilder } from "axe-webdriverjs";
 
 describe("Smoke", () => {
-  let webdriver = null;
+  let webdriver: WebDriver;
   beforeAll(async () => {
     webdriver = await new Builder().forBrowser("firefox").build();
   });
   beforeEach(async () => {
-    await webdriver.get("http://localhost:3000");
+    await webdriver!.get("http://localhost:3000");
   });
 
   it("Title", async () => {
-    await webdriver.wait(until.titleIs("Condor Club"), 1000);
+    await webdriver!.wait(until.titleIs("Condor Club"), 1000);
   });
 
   it("Axe", () => {
     return new Promise(resolve => {
-      axeBuilder(webdriver).analyze(result => {
+      AxeBuilder(webdriver!).analyze(result => {
         expect(result.violations).toHaveLength(0);
         resolve();
       });
@@ -24,6 +24,6 @@ describe("Smoke", () => {
   });
 
   afterAll(async () => {
-    webdriver.quit();
+    webdriver!.quit();
   });
 });
