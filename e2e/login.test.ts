ec77560031b70aa9f8abe47b6c9251e199a4125f
Nigel Schuster <Neitsch@users.noreply.github.com>
2018-07-20 03:44:28 +0200

    Login & Logout E2E test
    * Navigate to login page
    * Enter username & password
    * Click login
    * Wait to be redirected to home page
    * Click logout
    * Wait until logout page appears

---
+++ e2e/login.test.ts
@@ -0,0 +1,52 @@
+import { Builder, By, until, WebDriver } from "selenium-webdriver";
+// import axe, { AxeBuilder } from "axe-webdriverjs";
+
+const email = process.env["SAMPLE_EMAIL"];
+const password = process.env["SAMPLE_PASSWORD"];
+
+describe("Smoke", () => {
+  let webdriver: WebDriver;
+  beforeAll(async () => {
+    webdriver = await new Builder().forBrowser("firefox").build();
+  });
+  beforeEach(async () => {
+    await webdriver!.get("http://localhost:3000/auth/sign-in");
+    await webdriver!.wait(function() {
+      return webdriver!
+        .executeScript("return document.readyState")
+        .then(function(readyState) {
+          return readyState === "complete";
+        });
+    });
+  });
+
+  it(
+    "Login and Logout",
+    async () => {
+      // Login part
+      await webdriver!.wait(until.elementLocated(By.name("email")));
+      await webdriver!.wait(until.elementLocated(By.name("password")));
+      await webdriver!.wait(until.elementLocated(By.name("submit")));
+      let emailComponent = await webdriver!.findElement(By.name("email"));
+      let passwordComponent = await webdriver!.findElement(By.name("password"));
+      let submitComponent = await webdriver!.findElement(By.name("submit"));
+      await webdriver!.wait(until.elementIsVisible(emailComponent));
+      await webdriver!.wait(until.elementIsVisible(passwordComponent));
+      await webdriver!.wait(until.elementIsVisible(submitComponent));
+      await emailComponent.sendKeys(email);
+      await passwordComponent.sendKeys(password);
+      await submitComponent.click();
+      await webdriver!.wait(until.urlIs("http://localhost:3000/"), 20000);
+
+      // Logout part
+      await webdriver!.wait(until.elementLocated(By.id("accountButton")));
+      await webdriver!.findElement(By.id("accountButton")).click();
+      await webdriver!.findElement(By.id("logout")).click();
+      await webdriver!.wait(until.elementLocated(By.id("login")));
+    },
+    40000,
+  );
+  afterAll(async () => {
+    webdriver!.quit();
+  });
+});
