3fd1ad34bf7a48b5ae4eaa63f1b4e35da59f2d83
Nigel Schuster <Neitsch@users.noreply.github.com>
2018-07-23 16:27:54 +0200

    Add mocks

---
+++ __unit_mocks__/graphql.mock.ts
@@ -0,0 +1,5 @@
+export const GraphQLID = jest.fn();
+export const GraphQLNonNull = jest.fn();
+export const GraphQLObjectType = jest.fn();
+export const GraphQLString = jest.fn();
+export const GraphQLSchema = jest.fn();
