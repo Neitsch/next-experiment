eb7c8427b0b208a00fb8802e63123a481b823bf5
Nigel Schuster <Neitsch@users.noreply.github.com>
2018-08-29 22:41:42 -0700

    Remove log statements

--- e2e/home.test.ts
+++ e2e/home.test.ts
@@ -9,15 +9,10 @@ describe("Smoke", () => {
   beforeAll(async () => {
     const opts = new firefox.Options();
     opts.headless();
-    console.log("Pre setup");
     let inst = new Builder();
-    console.log("Started Builder");
     inst = inst.forBrowser("firefox");
-    console.log("Configured browser");
     inst = inst.setFirefoxOptions(opts);
-    console.log("Set opts");
     webdriver = await inst.build();
-    console.log("Awaited webdriver");
   });
   beforeEach(async () => {
     await webdriver!.get("http://localhost:3000");
